c2fd8563a84cbddfd71eac39e7e40342bbb4ce56
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-05-12 21:05:17 +0200

    fix: error

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import ViteRestart from 'vite-plugin-restart';
 // TODO: import legacy from '@vitejs/plugin-legacy' ? (https://nystudio107.com/blog/using-vite-js-next-generation-frontend-tooling-with-craft-cms)
 
 export default ({ command }) => ({
+    // 'vite' is always command = 'serve', other command is 'build'
     base: command === 'serve' ? '' : '/dist/',
     build: {
         manifest: true,
         outDir: '../cms/web/dist/',
         rollupOptions: {
             input: {
-                app: './src/js/app.ts',
+                app: './src/js/app.js',
             }
         },
     },
