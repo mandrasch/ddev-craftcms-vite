19ac335a30121168e41ae0938497592269231ee0
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-06-09 19:26:20 +0200

    feat: add support for github codespaces (WIP)

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+set -ex
+
+# ddev default commands
+# (see: https://ddev.readthedocs.io/en/latest/users/install/ddev-installation/#github-codespaces)
+ddev config global --omit-containers=ddev-router
+ddev config --auto && ddev debug download-images
+
+# setup our repo
+ddev start -y
+
+# replace primary site url with GitHub Codespaces URL (dynamic)
+ddev exec 'sed -i "/PRIMARY_SITE_URL=/c APP_URL=$DDEV_PRIMARY_URL" .env'
+
+ddev composer install
+ddev npm install
+
+ddev craft install/craft --interactive=0 --username=admin --password=NewPassword --email=admin@example.com --site-name=Testsite
--- config/vite.php
+++ config/vite.php
@@ -2,10 +2,19 @@
 
 use craft\helpers\App;
 // https://nystudio107.com/docs/vite/#using-ddev
+
+// https://github.com/orgs/community/discussions/5104
+// https://docs.github.com/en/codespaces/developing-in-codespaces/default-environment-variables-for-your-codespace
+$devServerPublic = App::env('PRIMARY_SITE_URL') . ':3000'; // default, local DDEV
+if (getenv('CODESPACES') !== false) {
+	// for codespaces
+	$devServerPublic = 'https://' . getenv('CODESPACE_NAME') . '-3000' . getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN');
+}
+
 return [
 	'checkDevServer' => true,
 	'devServerInternal' => 'http://localhost:3000',
-	'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3000',
+	'devServerPublic' => $devServerPublic,
 	'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
 	'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 	// other config settings...
