adc23b89ca909a27f9310868ba998d6dfd110437
Matthias Andrasch <matthias.andrasch@dotsandlines.io>
2023-06-12 10:03:51 +0200

    another try, WIP - not working yet (via https://github.com/vitejs/vite/pull/677#issuecomment-1473740472)

--- config/vite.php
+++ config/vite.php
@@ -5,15 +5,15 @@ use craft\helpers\App;
 
 // https://github.com/orgs/community/discussions/5104
 // https://docs.github.com/en/codespaces/developing-in-codespaces/default-environment-variables-for-your-codespace
-$devServerPublic = App::env('PRIMARY_SITE_URL') . ':3000'; // default, local DDEV
+$devServerPublic = App::env('PRIMARY_SITE_URL') . ':5173'; // default, local DDEV
 if (getenv('CODESPACES') !== false) {
 	// for codespaces
-	$devServerPublic = 'https://' . getenv('CODESPACE_NAME') . '-3000.' . getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN');
+	$devServerPublic = 'https://' . getenv('CODESPACE_NAME') . '-5173.' . getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN');
 }
 
 return [
 	'checkDevServer' => true,
-	'devServerInternal' => 'http://localhost:3000',
+	'devServerInternal' => 'http://localhost:5173',
 	'devServerPublic' => $devServerPublic,
 	'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
 	'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@
 import ViteRestart from 'vite-plugin-restart';
 // TODO: import legacy from '@vitejs/plugin-legacy' ? (https://nystudio107.com/blog/using-vite-js-next-generation-frontend-tooling-with-craft-cms)
 
+// https://github.com/vitejs/vite/pull/677#issuecomment-1473740472
+const codespaceName = process.env['CODESPACE_NAME'];
+const codespaceDomain = process.env['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN'];
+const hmrPort = 5173;
+
+const hmrRemoteHost = codespaceName ? `${codespaceName}-${hmrPort}.${codespaceDomain}` : 'localhost';
+const hmrRemotePort = codespaceName ? 443 : hmrPort;
+const hmrRemoteProtocol = codespaceName ? 'wss' : 'ws';
+
+console.log({codespaceName, codespaceDomain, hmrRemoteHost, hmrRemotePort, hmrRemoteProtocol});
+
+// Last step is to set 5173 to PUBLIC in the codespace, else you will get a CORS error. 
+// Unfortunately you cannot specify this in the .devcontainer
+
 export default ({ command }) => ({
     // 'vite' is always command = 'serve', other command is 'build'
     base: command === 'serve' ? '' : '/dist/',
@@ -17,8 +31,16 @@ export default ({ command }) => ({
     },
     // for ddev:
     server: {
-        host: '0.0.0.0',
-        port: 3000
+        host: '0.0.0.0', // react to all incoming traffic
+        port: 5173,
+        // new - for codespaces:
+        // TODO: check if this works locally in DDEV as well...
+        hmr: {
+            protocol: hmrRemoteProtocol,
+            host: hmrRemoteHost,
+            port: hmrPort,
+            clientPort: hmrRemotePort
+        }
     },
     plugins: [
         ViteRestart({
