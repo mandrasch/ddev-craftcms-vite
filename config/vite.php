dc45d5d866a2d2c2e04575aaa80a52f58bd8d920
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-06-25 15:11:34 +0200

    update to port 5173

--- config/vite.php
+++ config/vite.php
@@ -4,8 +4,8 @@ use craft\helpers\App;
 // https://nystudio107.com/docs/vite/#using-ddev
 return [
 	'checkDevServer' => true,
-	'devServerInternal' => 'http://localhost:3000',
-	'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3000',
+	'devServerInternal' => 'http://localhost:5173',
+	'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':5173',
 	'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
 	'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 	// other config settings...
