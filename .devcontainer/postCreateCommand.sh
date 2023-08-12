c1331ecdd77cfe989ce243378a10c6b3a5847b10
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-08-12 11:05:11 +0200

    fix: workaround file creation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,11 @@
 #!/bin/bash
 set -ex
 
+# This file is called in three scenarios:
+# 1. fresh creation of devcontainer
+# 2. rebuild
+# 3. full rebuild
+
 # ddev default commands
 # see: https://ddev.readthedocs.io/en/latest/users/install/ddev-installation/#github-codespaces
 ddev config global --omit-containers=ddev-router
@@ -19,8 +24,8 @@ ddev poweroff
 # which will expose port 5174. This port needs to be set to  public + HTTPS manually in ports tab, 
 # otherwise CORS errors will occur. See config/vite.php and vite.config.js as well. 
 
-if [ -f ".ddev/docker-compose.codespaces-vite.yaml" ]; then
-
+if [ ! -f ".ddev/docker-compose.codespaces-vite.yaml" ]; then
+    echo "Creating vite port workaround file for port exposing ..."
     # create workaround file for port exposing (if it does not exist yet)
     # info: this file should not be commited to git since it shouldn't be used on local DDEV
     cat >.ddev/docker-compose.codespaces-vite.yaml <<EOL
@@ -39,9 +44,18 @@ ddev start -y
 # If this is not working in future, you can use this snippet for replacement:
 # ddev exec 'sed -i "/PRIMARY_SITE_URL=/c APP_URL=$DDEV_PRIMARY_URL" .env'
 
+# Current workaround for https://github.com/ddev/ddev/issues/5256, can be removed later
+ddev exec 'sed -i "s/preview.app.github.dev/app.github.dev/g" .env'
+
 # normal project setup
 ddev composer install 
 ddev npm install
+
+# you could also perform a `ddev pull` here to get an existing remote database 
+
+# the following is just an example, you could also remove it here and run this in codespaces terminal,
+# it will just prompt for your password:
+# ddev craft install/craft --interactive=0 --username=admin --email=admin@example.com --site-name=Testsite
 
 # install craft via CLI
 ddev craft install/craft \
@@ -52,5 +66,6 @@ ddev craft install/craft \
   --site-name=Testsite
 
 # install the vite plugin by nystudio107
+# TODO: remove later, save activation of plugin in project config 
 ddev craft plugin/install vite
 
